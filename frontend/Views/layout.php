7330b9c1432543b6d5458f01dd82273d56e52df7
Asdrubal Hernandez <maxilive4@gmail.com>
2018-09-08 14:08:07 -0500

    Maquetando el sitio

--- frontend/Views/layout.php
+++ frontend/Views/layout.php
@@ -5,9 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="title" content="Tienda Virtual">
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci alias assumenda autem commodi deleniti distinctio, dolores in natus nemo nostrum praesentium quaerat quo reiciendis repellat reprehenderit temporibus ullam velit!">
+    <meta name="keyword" content="Lorem, ipsum, dolor, sit, amet, consectetur, adipisicing, elit, Ab, adipisci, alias, assumenda, autem commodi ">
+    
     <title>Proyecto E-Commerce</title>
+
+    <link rel="stylesheet" href="Views/css/plugins/font-awesome.min.css">
+    <link rel="stylesheet" href="Views/css/plugins/bootstrap.min.css">
 </head>
 <body>
-    <h1>Titulo</h1>
+
+    <!--=====================================================
+                            HEADER
+    ======================================================-->
+
+    <?php
+
+        include 'modulos/header.php';
+
+    ?>
+
+    <script src="Views/js/plugins/jquery.min.js"></script>
+    <script src="Views/js/plugins/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ frontend/Views/modulos/header.php
@@ -0,0 +1,199 @@
+
+<!--=====================================================
+                            TOP
+======================================================-->
+
+
+<div class="container-fluid barraSuperior" id="top">
+    <div class="container">
+        <div class="row">
+
+            <!--==== START SECTION SOCIAL ====-->
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+
+                <ul>
+                    <li>
+                        <a href="http://facebook.com/" target="_blank">
+                            <i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="http://youtube.com/" target="_blank">
+                            <i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="http://google.com/" target="_blank">
+                            <i class="fa fa-google-plus redSocial googleBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="http://instagram.com/" target="_blank">
+                            <i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+            <!--==== END SECTION SOCIAL ====-->
+
+            <!--==== START SECTION REGISTRO ====-->
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+                
+                <ul>
+                    <li>
+                        <a href="#modalIngreso" data-toggle="modal">Ingresar</a>
+                    </li>
+
+                    <li>|</li>
+
+                    <li>
+                        <a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a>
+                    </li>
+                </ul>
+                
+            </div>
+            <!--==== END SECTION REGISTRO ====-->
+        </div>
+    </div>
+</div>
+
+<!--=====================================================
+                           HEADER
+======================================================-->
+
+<header class="container-fluid">
+
+    <div class="container">
+
+        <div class="row" id="cabezote">
+
+            <!--==== START SECTION LOGOTIPO ====-->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+                <a href="#">
+                    <img src="http://localhost:8000/backend/Views/img/plantilla/logo.png" alt="" class="img-responsive">
+                </a>
+            </div>
+            <!--==== END SECTION LOGOTIPO ====-->
+
+            <!--==== START SECTION CATEGORIAS Y BUSCADOR ====-->
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+
+                <!--==== START SECTION CATEGORIA ====-->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+                    <p>
+                        CATEGORIAS
+                        <span class="pull-right">
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+                        </span>
+                    </p>
+                </div>
+                <!--==== END SECTION CATEGORIA ====-->
+
+                <!--==== START SECTION BUSCADOR ====-->
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+                    <input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+                    <span class="input-group-btn">
+                        <a href="#">
+                            <button class="btn btn-default backColor" type="submit">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </a>
+                    </span>
+                </div>
+                <!--==== END SECTION BUSCADOR ====-->
+
+            </div>
+            <!--==== END SECTION CATEGORIAS Y BUSCADOR ====-->
+
+            <!--==== START SECTION CARRITO ====-->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+                <a href="#">
+                    <button class="btn btn-default pull-left backColor">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </button>
+                </a>
+                <p>
+                    TU CESTA <span class="cantidadCesta"></span> <br> USD $ <span class="sumaCesta"></span>
+                </p>
+            </div>
+            <!--==== END SECTION CARRITO ====-->
+
+        </div>
+
+        <!--==== START SECTION CATEGORIAS ====-->
+        <div class="col-xs-12 backColor" id="categorias">
+
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+                
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategorias">Lorem Ipsum</a>
+                </h4>
+
+                <hr>
+
+                <ul>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                    <li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <!--==== END SECTION CATEGORIAS ====-->
+
+    </div>
+
+</header>
+
+
+
